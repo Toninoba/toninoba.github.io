5ae78c994e941ec6e0bacba82c47b554e892518d
Darya-Dyuzheva <104188013+Darya-Dyuzheva@users.noreply.github.com>
2024-06-04 00:44:44 +0200

    Version with Availability

---
+++ script_availability.js
@@ -0,0 +1,278 @@
+const jsonData ={
+    "devices": [
+      {
+        "name": "GE LOGIQ 7",
+        "room": 328,
+        "sonden": ["Typ 1", "Typ 2"],
+        "schedule": [
+          {
+            "date": "2024-05-31",
+            "availability": [
+              { "time": "09:00", "status": "blocked" },
+              { "time": "10:00", "status": "available" },
+              { "time": "11:00", "status": "available" },
+              { "time": "12:00", "status": "available" },
+              { "time": "13:00", "status": "blocked" },
+              { "time": "14:00", "status": "blocked" },
+              { "time": "15:00", "status": "available" },
+              { "time": "16:00", "status": "available" }
+            ]
+          },
+          {
+            "date": "2024-06-01",
+            "availability": [
+              { "time": "09:00", "status": "blocked" },
+              { "time": "10:00", "status": "available" },
+              { "time": "11:00", "status": "available" },
+              { "time": "12:00", "status": "available" },
+              { "time": "13:00", "status": "blocked" },
+              { "time": "14:00", "status": "blocked" },
+              { "time": "15:00", "status": "available" },
+              { "time": "16:00", "status": "available" }
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Philips EPIQ CVx",
+        "room": 329,
+        "sonden": ["Typ 3", "Typ 4"],
+        "schedule": [
+          {
+          
+            "date": "2024-06-01",
+            "availability": [
+              { "time": "09:00", "status": "available" },
+              { "time": "10:00", "status": "available" },
+              { "time": "11:00", "status": "available" },
+              { "time": "12:00", "status": "blocked" },
+              { "time": "13:00", "status": "available" },
+              { "time": "14:00", "status": "available" },
+              { "time": "15:00", "status": "blocked" },
+              { "time": "16:00", "status": "available" }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+  
+  
+  
+  const isLeapYear = (year) => {
+    return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
+  };
+  
+  const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+  };
+  
+  const month_names = [
+    'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
+  ];
+  
+  
+  let calendar = document.querySelector('.calendar');
+  let month_picker = document.querySelector('#month-picker');
+  const dayTextFormate = document.querySelector('.day-text-formate');
+  const timeFormate = document.querySelector('.time-formate');
+  const dateFormate = document.querySelector('.date-formate');
+  
+  month_picker.onclick = () => {
+    month_list.classList.remove('hideonce');
+    month_list.classList.remove('hide');
+    month_list.classList.add('show');
+  
+  };
+  
+  const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+      31, getFebDays(year), 31, 30, 31, 30,
+      31, 31, 30, 31, 30, 31
+    ];
+  
+    let currentDate = new Date();
+  
+    month_picker.innerHTML = month_names[month];
+    calendar_header_year.innerHTML = year;
+  
+    // Define German week days
+    const week_days_german = [
+      'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag',
+      'Freitag', 'Samstag', 'Sonntag'
+    ];
+  
+    // Select the element to add week days
+    let calendar_week_days = document.querySelector('.calendar-week-days');
+  
+    // Clear the calendar_week_days element before adding week days
+    calendar_week_days.innerHTML = '';
+  
+    // Add week days if they are not already added
+    if (calendar_week_days.children.length === 0) {
+      for (let i = 0; i < 7; i++) {
+        let weekday = document.createElement('div');
+        weekday.textContent = week_days_german[i];
+        calendar_week_days.appendChild(weekday);
+      }
+    }
+  
+    // Calculate the first day of the month
+    let first_day = new Date(year, month, 1);
+    let first_day_index = first_day.getDay();
+  
+    first_day_index = (first_day_index === 0) ? 6 : first_day_index - 1;
+  
+    first_day.setDate(first_day.getDate() - first_day_index);
+  
+    for (let i = 0; i < days_of_month[month] + first_day_index; i++) {
+      let day = document.createElement('div');
+      day.classList.add('calendar-day');
+  
+      if (i >= first_day_index) {
+        day.innerHTML = i - first_day_index + 1;
+  
+        if (i - first_day_index + 1 === currentDate.getDate() &&
+          year === currentDate.getFullYear() &&
+          month === currentDate.getMonth()
+        ) {
+          day.classList.add('current-date');
+        }
+  
+        day.addEventListener('click', () => {
+          document.querySelectorAll('.calendar-day.selected').forEach(d => {
+            d.classList.remove('selected');
+          });
+          day.classList.add('selected');
+          
+          let selectedDateDisplay = document.querySelector('.selected-date-display');
+          selectedDateDisplay.textContent = `Datum: ${day.innerHTML} ${month_names[month]} ${year}`;
+          
+          
+          const selectedDay = i - first_day_index + 1;
+          const selectedDate = [year, month+1, selectedDay];
+          
+          displayDeviceNames(selectedDate);
+          
+          
+      });
+      }
+      calendar_days.appendChild(day);
+    }
+  };
+  
+  
+  function displayDeviceNames(selectedDate) {
+      const deviceInfo = document.querySelector('.device-information');
+      deviceInfo.innerHTML = ''; 
+      jsonData.devices.forEach(device => {
+          // Check if the device has available slots for the selected date
+          const hasAvailableSlots = device.schedule.some(scheduleEntry => {
+              const [entryYear, entryMonth, entryDay] = scheduleEntry.date.split('-').map(Number);
+              return entryYear === selectedDate[0] && entryMonth === selectedDate[1] && entryDay === selectedDate[2] &&
+                     scheduleEntry.availability.some(slot => slot.status === 'available');
+          });
+  
+          if (hasAvailableSlots) {
+              const deviceElement = document.createElement('div');
+              deviceElement.classList.add('device', `room-${device.room}`);
+              deviceElement.textContent = `${device.name} [Raum: ${device.room}]`;
+              deviceElement.addEventListener('click', () => {
+                  toggleTimeSlots(device, selectedDate);
+                  toggleDeviceFrame(deviceElement);
+              });
+              deviceInfo.appendChild(deviceElement);
+          }
+          else {
+              const timeSlotsContainer = document.querySelector('.time-slots');
+              timeSlotsContainer.style.display = 'none';
+              clearBookingWindow();
+          }
+      });
+  }
+  
+  function toggleTimeSlots(device, selectedDate) {
+      const timeSlotsContainer = document.querySelector('.time-slots');
+      timeSlotsContainer.innerHTML = '';
+      device.schedule.forEach(scheduleEntry => {
+          const [entryYear, entryMonth, entryDay] = scheduleEntry.date.split('-').map(Number);
+          if (entryYear === selectedDate[0] && entryMonth === selectedDate[1] && entryDay === selectedDate[2]) {
+              scheduleEntry.availability.forEach(slot => {
+                  if (slot.status === 'available') {
+                      const slotElement = document.createElement('div');
+                      slotElement.classList.add('time-slot');
+                      slotElement.textContent = `Zeit: ${slot.time}`;
+                      const bookButton = document.createElement('button');
+                      bookButton.textContent = 'buchen'; // Button text
+                      bookButton.classList.add('book-button');
+                      bookButton.addEventListener('click', () => {
+                          displayBookingWindow(slot.time);
+                          bookButton.classList.add('active');
+                      });
+                      slotElement.appendChild(bookButton); // Append the button to the slot element
+                      timeSlotsContainer.appendChild(slotElement);
+                  }
+              });
+          }
+      });
+      timeSlotsContainer.style.display = 'block';
+  }
+  
+  function toggleDeviceFrame(deviceElement) {
+      const allDevices = document.querySelectorAll('.device');
+      allDevices.forEach(device => {
+          device.classList.remove('device-selected');
+      });
+      deviceElement.classList.add('device-selected');
+  }
+  
+  
+  function displayBookingWindow(time) {
+      const bookingWindow = document.querySelector('.booking-Window');
+      bookingWindow.textContent = 'Sie haben Ihren Termin erfolgreich gebucht!';
+      bookingWindow.style.display = 'block';
+  }
+  
+  function clearBookingWindow() {
+      const bookingWindow = document.querySelector('.booking-Window');
+      bookingWindow.textContent = ''; // Clear the content
+      bookingWindow.style.display = 'none'; // Hide the booking window
+  }
+  
+  
+  let month_list = calendar.querySelector('.month-list');
+  month_names.forEach((e, index) => {
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+  
+    month_list.append(month);
+    month.onclick = () => {
+      currentMonth.value = index;
+      generateCalendar(currentMonth.value, currentYear.value);
+      month_list.classList.replace('show', 'hide');
+    };
+  });
+  
+  (function() {
+    month_list.classList.add('hideonce');
+  })();
+  
+  document.querySelector('#pre-year').onclick = () => {
+    --currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+  };
+  
+  document.querySelector('#next-year').onclick = () => {
+    ++currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+  };
+  
+  let currentDate = new Date();
+  let currentMonth = { value: currentDate.getMonth() };
+  let currentYear = { value: currentDate.getFullYear() };
+  generateCalendar(currentMonth.value, currentYear.value);
+  
